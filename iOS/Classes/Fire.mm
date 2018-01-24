34f28bc3b32fb6634a1648bc33d0932f9b2c3607
Ryan Kontos <ryanjkontos@icloud.com>
2018-01-24 18:18:18 +1100

    Added flurry folder + eden.Eden

--- iOS/Classes/Fire.mm
+++ iOS/Classes/Fire.mm
@@ -317,6 +317,7 @@ Vector getFrameUV(int frame,int sprite)
 static int frame=0,frame2=0;
 void Fire::renderFireSprites()
 {
+    
    // glDepthMask(GL_TRUE);
     glDisable(GL_POINT_SPRITE_OES);
     glDisableClientState(GL_POINT_SIZE_ARRAY_OES);
